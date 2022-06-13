260d8c9ace892ac59db0556d76b4a5cfba2434ae
Rizbi Hassan <rizbiece@gmail.com>
2022-06-13 16:50:26 -0500

    update script

--- bin/cleanup-integration.sh
+++ bin/cleanup-integration.sh
@@ -5,18 +5,8 @@ export AWS_ACCESS_KEY_ID=$bamboo_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$bamboo_AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$bamboo_AWS_DEFAULT_REGION
 
-# your cleanup script here
-#clone cumulus orca template for deploying cumulus and orca
-git clone --branch $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION --single-branch https://git.earthdata.nasa.gov/scm/orca/cumulus-orca-deploy-template.git
-cd cumulus-orca-deploy-template
-echo "checked out to $bamboo_CUMULUS_ORCA_DEPLOY_TEMPLATE_VERSION branch"
 
-cd rds-cluster-tf
-echo "inside rds-cluster-tf"
-mv terraform.tfvars.example terraform.tfvars
-
-#replacing terraform.tf with proper values
-sed -e 's/PREFIX/'"$bamboo_PREFIX"'/g; s/us-east-1/'"$bamboo_AWS_DEFAULT_REGION"'/g' terraform.tf.example > terraform.tf
+aws s3 cp s3://rhassan-tf-state/delete/delete.tf .
 
 # Initialize deployment
 terraform init \
@@ -24,8 +14,10 @@ terraform init \
 
 
 # destroy 
-echo "destroying rds cluster module"
 terraform destroy \
   -auto-approve \
   -lock=false \
   -input=false
+
+
+  # remove other resources using aws cli if needed
