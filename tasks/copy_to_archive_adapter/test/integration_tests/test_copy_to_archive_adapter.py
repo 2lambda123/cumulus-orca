b71092b7268e1eaa855ad0933b6d1ffe1ea733dc
dornadigital@gmail.com <dornadigital@gmail.com>
2023-03-01 13:21:00 -0600

    Example run of selected tests

---
+++ tasks/copy_to_archive_adapter/test/integration_tests/test_copy_to_archive_adapter.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestCopyToArchiveAdapter(TestCase):
+    """
+    Test copy_to_archive_adapter functionality and business logic.
+    """
+    def test_fail(
+        self
+    ):
+        self.assertEqual(1, 0)
